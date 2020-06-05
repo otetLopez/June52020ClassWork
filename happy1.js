d2b78b711f4d2257135379eb7ac6ef277d95f5a7
otetLopez <otet.tud@gmail.com>
2020-06-05 10:52:19 -0400

    Initial File Printing Happy

---
+++ happy1.js
@@ -0,0 +1,10 @@
+// REPORT on the SUM of all numbers from 1000 going down to 17
+// Count down by 11 each time
+// ADD UP all the numbers you step over as you are counting down from 1000, 
+// going down 11 each time, until you get to 17
+// Output the result
+
+
+for (i=0;i<10;++i) {
+    console.log("Happy Summer!!!" + i);
+}
