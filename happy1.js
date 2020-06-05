9389b16cd7fd8c1868a8e43ccb18b9f71c621d6e
otetLopez <otet.tud@gmail.com>
2020-06-05 12:23:37 -0400

    adding web

--- happy1.js
+++ happy1.js
@@ -1,13 +1,30 @@
-
-sum = 0;
-for (i=0;i<10;++i) {
-    console.log("Happy Summer!!!" + i);
-    sum += i;
-    if(i == 5) {
-        break;
+// Function
+function a() {
+    var sum = 0;
+    for (i=0;i<10;++i) {
+       // console.log("Happy Summer!!!" + i);
+        sum += i;
+        if(i == 5) {
+            break;
+        }
     }
+    return sum;
 }
-console.log("sum is " + sum);
+
+// Function passing parameters
+function b(start, stop, stepvalue) {
+    var sum = 0;
+    for (i=start;i>stop;++i) {
+       console.log("Happy Summer!!!" + i);
+        sum = sum + stepvalue;
+        if(i == 5) {
+            break;
+        }
+    }
+    return sum;
+}
+// console.log("sum is " + a());
+console.log("sum is " + b(1000, 10, -17));
 
 
 
