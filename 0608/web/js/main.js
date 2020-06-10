3a3f9ea8c84a14d0bc0c0cc2788542e8b0affddb
otetLopez <otet.tud@gmail.com>
2020-06-10 08:45:55 -0400

    Commit everything unsaved last meeting and added new empty file for today 0610

--- 0608/web/js/main.js
+++ 0608/web/js/main.js
@@ -9,7 +9,9 @@ function summation(min, max, step) {
 return sum;
 }
 
+
 sum = summation(5, 100, 7);
+// sum =summation_2(1,100,6)
 console.log("This is the returned answer: " + sum);
 
 document.getElementById('sum').innerHTML = sum;
