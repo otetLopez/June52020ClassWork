eb5b0eaa29f3e892414c7cc28b5a989d7e4ca547
otetLopez <otet.tud@gmail.com>
2020-06-10 11:12:26 -0400

    Adding debug file

---
+++ 0610/debugEx.js
@@ -0,0 +1,11 @@
+var a,b,c;
+let x,y,z;
+
+a = 1;
+b = 2;
+c = 3;
+
+var count = 0;
+for ( ; true; ) {
+    console.log(++count + " The number is now " + a++ + " " + b++);
+}
--- 0610/site/script/main.js
+++ 0610/site/script/main.js
@@ -1,11 +1,13 @@
 function getSum(min, max, step) {
     var sum = 0;
-    for(i=max; i>=min; i-=step) {
+    for(i=max; i>min; ) {
         sum += i;
+        i = i-17;
     }
     return sum;
 }
 
 document.getElementById('answer').innerText = getSum(100, 1000, 17);
+console.log(getSum(100, 1000, 17));
 
 
